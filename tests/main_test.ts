adc8f96f05ac17a765d686bb4085877c71e4ebbb
Ravio Shankar <ravioshankar@outlook.com>
2024-03-31 21:56:19 -0500

    format

--- tests/main_test.ts
+++ tests/main_test.ts
@@ -1,7 +1,7 @@
 import { createHandler, ServeHandlerInfo } from "$fresh/server.ts";
 import manifest from "../fresh.gen.ts";
 import config from "../fresh.config.ts";
-import { assertEquals  } from "$std/testing/asserts.ts";
+import { assertEquals } from "$std/testing/asserts.ts";
 
 const CONN_INFO: ServeHandlerInfo = {
   remoteAddr: { hostname: "127.0.0.1", port: 53496, transport: "tcp" },
@@ -12,6 +12,6 @@ Deno.test("HTTP assert test.", async (t) => {
 
   await t.step("#1 GET /", async () => {
     const resp = await handler(new Request("http://127.0.0.1/"), CONN_INFO);
-    assertEquals (resp.status, 200);
+    assertEquals(resp.status, 200);
   });
 });
